ac7218e75b020159331f41b9c42461ff6c634a50
Anton Malay <romory999@gmail.com>
2025-08-18 22:46:45 +0300

    some fix

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -4,12 +4,6 @@ import { IonReactRouter } from "@ionic/react-router";
 import { Route, Redirect, Switch } from "react-router-dom";
 
 import { AppFooter, AppHeader } from "@widgets/layout";
-import {
-  ActivitiesList,
-  ActivityDetail,
-  CreateActivity,
-  EditActivity,
-} from "@pages/index";
 
 export const AppRoutes: FC = () => {
   return (
@@ -18,12 +12,8 @@ export const AppRoutes: FC = () => {
         <AppHeader />
         <IonRouterOutlet>
           <Switch>
-            <Route exact path="/activities" component={ActivitiesList} />
-            <Route exact path="/activity/create" component={CreateActivity} />
-            <Route exact path="/activity/:id/edit" component={EditActivity} />
-            <Route exact path="/activity/:id" component={ActivityDetail} />
             <Route exact path="/">
-              <Redirect to="/activities" />
+              <Redirect to="/walks" />
             </Route>
           </Switch>
         </IonRouterOutlet>
--- src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { ActivitiesList } from "./activities-list/ui";
-export { ActivityDetail } from "./activity-details/ui";
-export { CreateActivity } from "./activity-create/ui";
-export { EditActivity } from "./activity-edit/ui";
